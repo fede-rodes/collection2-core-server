edcc36cf7a4d8bae7ced2c820ba5e1f9db09d3d9
fede-rodes <rodesf@gmail.com>
2018-01-24 09:49:44 -0300

    bumping collection2-core version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## orcprogramming:collection2-core-server
-Wrapper around [aldeed:collection2-core](https://atmospherejs.com/aldeed/collection2-core) to limit the scope of said package to the server. As a results, both Collection2 and
+Wrapper around [aldeed:collection2-core](https://atmospherejs.com/aldeed/collection2-core) to limit the scope of said package to be server side only. As a results, both Collection2 and
 simpl-schema are removed from the client bundle resulting in a saving of 47.1KB.
 
 ## Installation
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'orcprogramming:collection2-core-server',
-  version: '0.0.4',
-  summary: 'Limit the scope of aldeed:collection2-core to the server',
+  version: '0.0.5',
+  summary: 'Limits the scope of aldeed:collection2-core to be server side only',
   git: 'https://github.com/fede-rodes/collection2-core-server.git',
   documentation: 'README.md'
 });
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.use('ecmascript@0.6.1');
   // Limit the scope of aldeed:collection2-core to be server side only
-  api.use('aldeed:collection2-core@2.1.0', 'server');
+  api.use('aldeed:collection2-core@2.1.1', 'server');
   api.mainModule('collection2-core-server.js', 'server');
   api.export('Collection2', 'server');
 });
